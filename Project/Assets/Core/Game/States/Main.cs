8d5ee4cb23d9226edff7ae3d34e5a87080aac008
oliperraul <olivier.perrault@usherbrooke.ca>
2019-01-25 23:49:13 -0500

    start

---
+++ Project/Assets/Core/Game/States/Main.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Core.FSM;
+
+namespace Core.States
+{
+    public class Main : Resource
+    {
+        public override int Id => (int)States.Id.Main;
+
+        public override FSM.State Create(object[] context)
+        {
+            return new State(context, this);
+        }
+
+        public class State : States.State
+        {
+            public State(object[] context, Core.FSM.Resource resource) : base(context, resource) { }
+        }
+
+    }
+}
---
+++ Project/Assets/Core/UI/ButtonsManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsManager : MonoBehaviour
+{
+    public void OnStart()
+    {
+        Core.Game.Instance.Machine.SetState(Core.States.Id.Start);
+    }
+
+}
