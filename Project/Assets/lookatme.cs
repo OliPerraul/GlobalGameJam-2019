24b4b7e2917f8400682b42a463af2508f0667c3d
oliperraul <olivier.perrault@usherbrooke.ca>
2019-01-27 13:25:59 -0500

    asdasdas

--- Project/Assets/lookatme.cs
+++ Project/Assets/lookatme.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class lookatme : MonoBehaviour
 {
+    [SerializeField]
+    CompleteProject.CameraFollow foolow;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    //var targetObj : GameObject;
+    float speed  = 5;
+ 
+
+
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (foolow != null && foolow.target != null)
+        {
+            Vector3 p = Vector3.zero;
+
+            if (foolow.target2 != null)
+            {
+                p = (foolow.target.transform.position + foolow.target2.transform.position) / 2;
+
+            }
+            else
+            {
+                p = foolow.target.transform.position;
+            }
+
+
+            var targetRotation = Quaternion.LookRotation(p - transform.position);
+
+            // Smoothly rotate towards the target point.
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+
+
+
+
+
+
+        }
+     }
 }
